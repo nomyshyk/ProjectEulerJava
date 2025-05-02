4bde5a9b702d1391625a786af49153fb9fc0cdb8
Alisher_Kasymov <kasiom@gmail.com>
2025-05-02 11:32:03 -0500

    problem 10

--- src/main/java/kg/euler/Day0010.java
+++ src/main/java/kg/euler/Day0010.java
@@ -7,22 +7,22 @@ public class Day0010 {
 
     static Set<Integer> checked = new HashSet<>();
     public static void main(String[] args) {
-        //solution(2000000);
-        solution(500000);
+        solution(2000000);
+        //solution(10);
     }
 
     //Find the sum of all the primes below two million.
     static long solution(int maxValue) {
         long result = 0L;
-        outer:
+
        for(int i = 2; i < maxValue; i++){
-
-           //IntStream.range(i, maxValue).filter(value -> value);
-
+           if(checked.contains(i)) {
+               continue;
+           }
            if(checkPrime(i)){
                result += i;
            }
-//           checked.add(i);
+           tableEratosfenSet(checked,i,maxValue);
        }
         System.out.println(result);
        return result;
@@ -35,5 +35,12 @@ public class Day0010 {
             }
         }
         return true;
+    }
+
+    static Set<Integer> tableEratosfenSet(Set<Integer> ints, int numberToAdd, long limit) {
+        for(int i = numberToAdd; i < limit; i+=numberToAdd) {
+            ints.add(i);
+        }
+        return ints;
     }
 }
