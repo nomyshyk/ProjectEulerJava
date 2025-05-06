092901b824d473e3f3e823a0dd194545736ee977
kasiom <kasiom@gmail.com>
2025-05-05 23:37:19 -0500

    problem 17 update

--- src/main/java/kg/euler/Day0017.java
+++ src/main/java/kg/euler/Day0017.java
@@ -1,13 +1,7 @@
 package kg.euler;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 public class Day0017 {
     static String vals = """
