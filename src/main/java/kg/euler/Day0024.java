282e9c0a99662973f8cf44bccdb614aa061f1d42
Alisher_Kasymov <kasiom@gmail.com>
2025-05-09 17:04:05 -0500

    problem 24 update

--- src/main/java/kg/euler/Day0024.java
+++ src/main/java/kg/euler/Day0024.java
@@ -10,7 +10,7 @@ import java.util.Set;
 public class Day0024 {
 
     public static void main(String[] args) {
-        solution("012");
+        solution("120");
     }
 
     // What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
@@ -18,11 +18,30 @@ public class Day0024 {
 
         StringBuilder sb = new StringBuilder(permutatingStr);
         int idx = -1;
-        for(int i = permutatingStr.length() - 1; i > 0; i--) {
-            if(sb.charAt(i) > sb.charAt(i-1)) {
-                idx = i;
+        int max = -1;
+        boolean foundGreater = true;
+        int maxIdx = -1;
+        char val = 0;
+
+        while (foundGreater) {
+            for(int i = permutatingStr.length() - 1; i > 0; i--) {
+                if(sb.charAt(i) > sb.charAt(i-1) && idx == -1) {
+                    idx = i-1;
+                    val = sb.charAt(i-1);
+                }
+                if(idx >= 0 && val < sb.charAt(i)) {
+                    if(max < sb.charAt(i)) {
+                        max = sb.charAt(i);
+                        maxIdx = i;
+                    }
+                }
             }
+            System.out.printf("Idx = %s, maxIdx = %s, char[idx]=%s, char[maxIdx]=%s \n",
+                    idx, maxIdx, sb.charAt(idx), sb.charAt(maxIdx));
+            break;
         }
+
+
         System.out.println(idx);
         return 0;
     }
