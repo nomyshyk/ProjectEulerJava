cf6a78a423b0a75307f318d2da83de1aabf7b45f
kasiom <kasiom@gmail.com>
2025-05-10 01:00:33 -0500

    problem 24 solution

--- src/main/java/kg/euler/Day0024.java
+++ src/main/java/kg/euler/Day0024.java
@@ -1,48 +1,52 @@
 package kg.euler;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 public class Day0024 {
 
     public static void main(String[] args) {
-        solution("120");
+        solution("0123456789", 1000000);
     }
 
     // What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
-    static long solution(String permutatingStr) {
+    static String solution(String permutatingStr, int limit) {
 
         StringBuilder sb = new StringBuilder(permutatingStr);
-        int idx = -1;
-        int max = -1;
-        boolean foundGreater = true;
+
         int maxIdx = -1;
         char val = 0;
+        int permCnt = 1;
 
-        while (foundGreater) {
+        while (permCnt < limit) {
+            int idx = -1;
             for(int i = permutatingStr.length() - 1; i > 0; i--) {
                 if(sb.charAt(i) > sb.charAt(i-1) && idx == -1) {
                     idx = i-1;
                     val = sb.charAt(i-1);
                 }
-                if(idx >= 0 && val < sb.charAt(i)) {
-                    if(max < sb.charAt(i)) {
-                        max = sb.charAt(i);
-                        maxIdx = i;
-                    }
+            }
+
+            if(idx == -1) {
+                break;
+            }
+
+            for(int i = permutatingStr.length() - 1; i >= idx; i--) {
+                if(val < sb.charAt(i)) {
+                    maxIdx = i;
+                    break;
                 }
             }
-            System.out.printf("Idx = %s, maxIdx = %s, char[idx]=%s, char[maxIdx]=%s \n",
-                    idx, maxIdx, sb.charAt(idx), sb.charAt(maxIdx));
-            break;
+
+            StringBuilder temp = new StringBuilder(sb);
+            temp.setCharAt(idx, sb.charAt(maxIdx));
+            temp.setCharAt(maxIdx, sb.charAt(idx));
+            String substringEnd = temp.substring(idx+1, temp.length());
+            StringBuilder sb2 = new StringBuilder(substringEnd).reverse();
+            String substringBeg = temp.substring(0, idx+1);
+            permCnt++;
+
+            sb = new StringBuilder(substringBeg + sb2);
         }
-
-
-        System.out.println(idx);
-        return 0;
+        
+        System.out.println(sb);
+        return sb.toString();
     }
 }
