aeef3a8bb8e8599efdb0b848d565c0f6ab987873
kasiom <kasiom@gmail.com>
2025-05-19 23:09:28 -0500

    problem 27 solved

--- src/main/java/kg/euler/Day0027.java
+++ src/main/java/kg/euler/Day0027.java
@@ -1,16 +1,14 @@
 package kg.euler;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Day0027 {
 
     static Map<Long, Boolean> setka = new HashMap<>();
-    //static Map<Long, Boolean> cache = new HashMap<>();
+
     public static void main(String[] args) {
-        long result = solution(1000);
+        long result = solution(999);
         System.out.println("result is " + result);
     }
 
@@ -18,17 +16,16 @@ public class Day0027 {
     // for the quadratic expression that produces the maximum number of primes
     // for consecutive values of n, starting with n = 0.
     static long solution(int limit) {
-        List<Integer> primesList = getPrimesList(limit);
         long maxLen = 0;
         int resA = 0;
         int resB = 0;
-        for(int i = 0; i < primesList.size(); i++) {
-            for(int j = -999; j <= 999; j++) {
-                long lengthFound = formula(j, primesList.get(i));
+        for(int i = -limit; i <= limit; i++) {
+            for(int j = -limit; j <= limit; j++) {
+                long lengthFound = formula(i, j);
                 if(lengthFound > maxLen) {
                     maxLen = lengthFound;
                     resA = j;
-                    resB = primesList.get(i);
+                    resB = i;
                 }
             }
         }
@@ -40,11 +37,9 @@ public class Day0027 {
 
     static long formula(int a, int b) {
         int cnt = 0;
-
-        for(int i = 0; ; i++) {
-            long result = ((long)i * i) + ((long)a * i) + b;
-            //System.out.printf("a=%s, b=%s, n=%s, res=%s \n", a, b, i, result);
-            if(isPrime(Math.abs(result))) {
+        for(int n = 0; ; n++) {
+            long result = n * n + a * n + b;
+            if(isPrime(result)) {
                 cnt++;
             } else {
                 return cnt;
@@ -52,18 +47,11 @@ public class Day0027 {
         }
     }
 
-    static List<Integer> getPrimesList(int maxLimit) {
-        List<Integer> primesList = new ArrayList<>();
-
-        for (int i = 3; i <= maxLimit; i++){
-            if(isPrime(i)){
-                primesList.add(i);
-            }
-        }
-        return primesList;
-    }
-
     static boolean isPrime(long num) {
+        if(num < 2) {
+            return false;
+        }
+
         Boolean isPrime = setka.get(num);
         if(isPrime != null) {
             return isPrime;
